b5702e660022b70abb48fe137fc53a412b7ea59b
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2021-12-14 18:48:41 +0100

    async frissítés

--- Vizsgaremek/Felszolgalo/FelszolgaloUI.xaml.cs
+++ Vizsgaremek/Felszolgalo/FelszolgaloUI.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Vizsgaremek.Osztalyok;
 
 namespace Vizsgaremek.Felszolgalo
 {
@@ -22,6 +24,36 @@ namespace Vizsgaremek.Felszolgalo
         public FelszolgaloUI()
         {
             InitializeComponent();
+            Task.Run(() => keszListboxFrissitAsync());
+            
+        }
+
+        private void keszListboxFrissit()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                zartListBox.Items.Clear();
+                List<string> keszRendelesek = MySQL.query("pincerkeszfrissit", false);
+                if (keszRendelesek.Any())
+                    for (int i = 0; i < keszRendelesek.Count; i += 3)
+                    {
+                        string rendeles = $"{keszRendelesek[i]} {keszRendelesek[i + 1]} {keszRendelesek[i + 2]}";
+                        zartListBox.Items.Add(rendeles);
+                    }
+                
+            });
+            
+        }
+
+        private async void keszListboxFrissitAsync()
+        {
+
+            while (true)
+            {
+                keszListboxFrissit();
+                await Task.Delay(5000);
+            }
+            
         }
 
     }
