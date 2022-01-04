f22c5d92d0a69c762392966ebe0041e49363bd7b
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-01-04 13:22:31 +0100

    szakacsUI + bejelentkezesUI

--- Vizsgaremek/Szakacs/SzakacsUI.xaml.cs
+++ Vizsgaremek/Szakacs/SzakacsUI.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Vizsgaremek.Osztalyok;
+using MySql.Data.MySqlClient;
 
 namespace Vizsgaremek.Szakacs
 {
@@ -53,13 +54,38 @@ namespace Vizsgaremek.Szakacs
                 .Where(x => x.etelstatus == 1)
                 .OrderByDescending(x => x.ido))
             {
-                foreach (Tetel t in r.tetelek)
+                foreach (Tetel t in r.tetelek.Where(x => x.etelstatus == 1))
                 {
                     StackPanel sp = StackPanelExpander.tetelElemKeszit(t);
                     if(!tetelekListBox.Items.OfType<StackPanel>().Any(x => (int)x.Tag == (int)sp.Tag))
                         tetelekListBox.Items.Add(sp);
                 }
             }
+        }
+        private void tetelKesz(object sender, RoutedEventArgs e)
+        {
+            StackPanel tetel = (StackPanel)tetelekListBox.SelectedItem;
+            MessageBoxResult result = MessageBox.Show($"Biztosan készen van a {tetel.Tag}. számú tétel?", "Készen van?", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                MySqlParameter tazon = new("@tazon", tetel.Tag);
+                List<MySqlParameter> keszTetelParams = new() { tazon };
+                List<string> eredmeny = MySQL.query("etelkesz", true, keszTetelParams);
+                if (eredmeny.Any())
+                {
+                    tetelekListBox.Items.Remove(tetel);
+                    MessageBox.Show(eredmeny[0]);
+                }
+            }
+
+        }
+        private void tetelekListBoxValasztas(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            if (lb.SelectedItem is not null)
+                tetelKeszGomb.IsEnabled = true;
+            else
+                tetelKeszGomb.IsEnabled = false;
         }
 
         private void kijelentkezes(object sender, RoutedEventArgs e)
@@ -69,5 +95,7 @@ namespace Vizsgaremek.Szakacs
             bejelentkezes.Show();
             Close();
         }
+
+
     }
 }
